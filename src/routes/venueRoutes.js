c44c0b14aea07796c8ea6f62fbceaff927972f42
Ndatimana - Assa <assandatimana15@gmail.com>
2026-08-30 21:10:45 +0200

    feat: protect venue write routes with JWT middleware

--- src/routes/venueRoutes.js
+++ src/routes/venueRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const venueController = require('../controllers/venueController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.get('/', venueController.getAllVenues);
 router.get('/:id', venueController.getVenueById);
-router.post('/', venueController.createVenue);
-router.put('/:id', venueController.updateVenue);
-router.delete('/:id', venueController.deleteVenue);
+router.post('/', protect, venueController.createVenue);
+router.put('/:id', protect, venueController.updateVenue);
+router.delete('/:id', protect, venueController.deleteVenue);
 
 module.exports = router;
